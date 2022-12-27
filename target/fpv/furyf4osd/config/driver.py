e60fd77c3204ecc3f52940f7eab1c0ff69957e19
gf_name <gf>
2022-12-27 14:40:25 +0800

    [add]furyf4osd manba

---
+++ target/fpv/furyf4osd/config/driver.py
@@ -0,0 +1,18 @@
+# Modify this file to decide which drivers are compiled
+
+DRIVERS = [
+    # 'imu/icm20689.c',
+    'imu/mpu6000.c',
+    # 'imu/bmi055.c',
+    # 'mag/ist8310.c',
+    # 'barometer/ms5611.c',
+    'barometer/spl06.c',
+    # 'gps/gps_m8n.c',
+    # 'rgb_led/ncp5623c.c',
+    # 'mtd/ramtron.c',
+    # 'vision_flow/mtf_01.c',
+    # 'airspeed/ms4525.c',
+      'mtd/w25qxx.c',
+]
+
+DRIVERS_CPPPATH = []
